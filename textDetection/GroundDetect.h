fdbb1d7c71a16154e69370437d1fdab23841e00d
yestinsong <yestinsong@gmail.com>
2015-05-20 12:45:06 +0800

    Update GroundDetect.h

--- textDetection/GroundDetect.h
+++ textDetection/GroundDetect.h
@@ -8,7 +8,7 @@
 #include"VericaficationTextLine.h"
 #include"Fmeasure.h"
 #include "headline.h"
-
+///Main class for text detection
 class GroundDetect{
 public:
 	void readImageGroundTruth(const cv::Mat& image,const std::vector<std::string> gt);
@@ -16,15 +16,15 @@ public:
 	void showPolyGroundTruthSet();
 	std::string calculateResult();
 private:
-	GetCandidate getCandidate;
-	LinkCandidate linkCandidate;
-	VericaficationTextLine vericaficationTextLine;
-	Fmeasure fmeasure;
+	GetCandidate getCandidate;///class for get all characters from input image
+	LinkCandidate linkCandidate;///class for link characters into text lines
+	VericaficationTextLine vericaficationTextLine;///class for text line vericafication
+	Fmeasure fmeasure;//class for calculating the F-measure based on cgts and pgts
 
-	cv::Mat oriBgrImage_8UC3;
-	std::vector<Candidate> candidateStore;
-	PolyGroundTruthSet cgts;
-	PolyGroundTruthSet pgts;
+	cv::Mat oriBgrImage_8UC3;///Copy of the input Image
+	std::vector<Candidate> candidateStore;///Character set calculated by the system
+	PolyGroundTruthSet cgts;///The Rect region calculdated by the system
+	PolyGroundTruthSet pgts;///The Rect region transform by gt
 	std::string PRF;///precision,recall,F-measure
 };
 
